66a0a109e6375cd2d1fa1378ede5727baf7077c5
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-10-09 15:31:14 -0400

    Converted the getTestDataArray function to a single-expression function using the 'apply' scope function

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         // You can test your helper functions by  calling them from onCreate() and
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
+        Log.d("function output", getTestDataArray().toString())
 
     }
 
@@ -27,10 +28,14 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
+//    private fun getTestDataArray() : List<Int> {
+//        val testArray = MutableList(10){ Random.nextInt()}
+//        testArray.sort()
+//        return testArray
+//    }
+
+    private fun getTestDataArray() = MutableList(10) { Random.nextInt()}.apply {
+        sort()
     }
 
     // Return true if average value in list is greater than median value, false otherwise
